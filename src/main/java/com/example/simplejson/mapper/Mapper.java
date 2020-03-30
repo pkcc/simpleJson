d324107778f40eaa07717122dd7dc1bd8c2c417b
张登凯 <1097574823@qq.com>
2020-03-30 20:06:17 +0800

    添加代码注释

--- src/main/java/com/example/simplejson/mapper/Mapper.java
+++ src/main/java/com/example/simplejson/mapper/Mapper.java
@@ -12,6 +12,10 @@ import java.util.List;
 import java.util.Map;
 
 public class Mapper {
+
+    private Mapper() {
+    }
+
     private static final Class<?>[] supportType = new Class[]{
             Integer.class,
             Long.class,
@@ -20,18 +24,17 @@ public class Mapper {
             Boolean.class,
             Character.class,
             String.class,
-
     };
 
-    private Mapper() {
-    }
-
+    /**
+     * 将Object对象转换成对应的 JsonObject，JsonObject本质上是一个HashMap
+     */
     public static JsonObject toJsonObject(Object object) {
         JsonObject jsonObject = new JsonObject();
         Map<String, ObjMethod> properties = getProperties(object.getClass());
         for (Map.Entry<String, ObjMethod> entry : properties.entrySet()) {
-            String key = entry.getKey();
-            Method getMethod = entry.getValue().getMethod;
+            String key = entry.getKey();    // key 是属性字段名
+            Method getMethod = entry.getValue().getMethod;  // 该属性对应的get方法
             Object value = null;
             try {
                 value = getMethod.invoke(object);
@@ -53,6 +56,9 @@ public class Mapper {
         return jsonObject;
     }
 
+    /**
+     * 将List数组转换成对应的 JsonArray，JsonArray本质上是一个ArrayList
+     */
     public static JsonArray toJsonArray(List<?> list) {
         JsonArray jsonArray = new JsonArray();
         for (Object o : list) {
@@ -73,7 +79,13 @@ public class Mapper {
         return jsonArray;
     }
 
-
+    /**
+     * 将一个JsonObject还原成指定类型的JavaBean对象
+     *
+     * @param jsonObject 需要还原的JsonObject对象
+     * @param clazz      JavaBean的类型
+     * @return 对应的JavaBean对象
+     */
     public static <T> T toBean(JsonObject jsonObject, Class<T> clazz) {
         try {
             T bean = clazz.getDeclaredConstructor().newInstance();
@@ -89,12 +101,14 @@ public class Mapper {
                     Object o = toBean((JsonObject) value, field.getType());
                     setMethod.invoke(bean, o);
                 } else if (value instanceof JsonArray) {
+                    // 这里是一个坑，需要获取该List所存放元素的具体类型，由于Java类型擦除机制，要获取该信息比较麻烦
                     String typename = ((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0].getTypeName();
                     List<?> l = toList((JsonArray) value, Class.forName(typename));
                     setMethod.invoke(bean, l);
                 } else if (value == null) {
                     //nothing to do
                 } else if (value instanceof String) {
+                    // 对于Json字符串中的字符串类型，只支持将其还原成String类型
                     Class<?> fieldType = field.getType();
                     if (fieldType == String.class) {
                         setMethod.invoke(bean, value);
@@ -124,6 +138,13 @@ public class Mapper {
         return null;
     }
 
+    /**
+     * 将一个JsonArray还原成存放指定类型元素的List集合
+     *
+     * @param jsonArray 需要还原的JsonArray对象
+     * @param clazz     List中存放元素的类型
+     * @return 存放指定类型元素的List集合
+     */
     public static <T> List<T> toList(JsonArray jsonArray, Class<T> clazz) {
         List<T> list = new ArrayList<>();
         for (Object value : jsonArray) {
@@ -148,12 +169,18 @@ public class Mapper {
         return list;
     }
 
+    /**
+     * 获取该Class的属性字段以及其对应的get、set方法
+     * 对于不满足JavaBean规范的属性字段将忽略
+     */
     private static Map<String, ObjMethod> getProperties(Class<?> clazz) {
         Map<String, ObjMethod> properties = new HashMap<>();
         for (Field field : clazz.getDeclaredFields()) {
-            if (Modifier.isStatic(field.getModifiers())) continue;
+            // 属性字段必须是private，且不能为static
+            if (Modifier.isStatic(field.getModifiers()) || !Modifier.isPrivate(field.getModifiers())) continue;
             String fieldName = field.getName();
             Class<?> fieldType = field.getType();
+            // 根据JavaBean规范，生成该字段所对应的get、set方法的名字。对于bool类型的字段，其get方法是以is开头
             String fieldNameHeaderUpper = fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
             String getMethodName = (fieldType == boolean.class || fieldType == Boolean.class ? "is" : "get")
                     + fieldNameHeaderUpper;
@@ -167,8 +194,9 @@ public class Mapper {
             } catch (NoSuchMethodException e) {
                 continue;
             }
-            if (Modifier.isStatic(getMethod.getModifiers())) continue;
-            if (!Modifier.isPublic(setMethod.getModifiers())) continue;
+            // 方法必须是public，且不能为static
+            if (Modifier.isStatic(getMethod.getModifiers()) || Modifier.isStatic(setMethod.getModifiers())) continue;
+            if (!Modifier.isPublic(getMethod.getModifiers()) || !Modifier.isPublic(setMethod.getModifiers())) continue;
             properties.put(fieldName, new ObjMethod(getMethod, setMethod));
         }
         return properties;
